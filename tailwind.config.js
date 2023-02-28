93d2b5a005c39984219c402fa77c7b286aac1abe
wecesscary <wesleymhj@gmail.com>
2023-02-28 18:06:14 +0000

    fix color: move to extend to keep default color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: { background: "#FBF9F9", ...defaultTheme.colors },
     extend: {
+      colors: { background: "#FBF9F9", ...defaultTheme.colors },
       fontFamily: {
         serif: ["var(--bodoni)", ...defaultTheme.fontFamily.serif],
       },
