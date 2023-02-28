a8d507cc506e29f6e97cda7431f9b5b3cb258502
wecesscary <wesleymhj@gmail.com>
2023-02-28 18:06:56 +0000

    add layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,29 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="font-serif h-screen w-screen">
-        <div className="text-9xl ">WESLEY JESSIE</div>
+      <main className="font-serif">
+        <section className="grid grid-rows-6 grid-flow-col min-h-screen">
+          <div className="row-span-1 border-b-2 self-end text-8xl">
+            WESLEY JESSIE
+          </div>
+          <div className="row-span-1 border-b-2 self-end text-end text-8xl">
+            FRONT END DEVELOPER
+          </div>
+          <div className="row-span-3 border-b-2">
+            <div className="grid grid-rows-3 grid-flow-col h-full">
+              <div className="row-span-1">
+                <div className="text-4xl">wesleymhj@gmail.com</div>
+                <div className="text-4xl">+44 7597296544</div>
+              </div>
+              <div className="row-span-2 self-end justify-self-end">
+                <div className="bg-black rounded-full w-64 h-64 relative">
+                  <div className="bg-[#D9D9D9] rounded-full w-12 h-12 top-14 left-2 absolute"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="text-8xl self-end">London, UK</div>
+        </section>
       </main>
     </>
   );
