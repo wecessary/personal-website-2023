a53fdaca014c985c2e4d2c428b2a35d5634a9172
wecesscary <wesleymhj@gmail.com>
2023-03-01 18:49:08 +0000

    apply background colour to body

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${bodoni.variable} font-serif bg-background`}>
+    <main className={`${bodoni.variable} font-serif`}>
       <Component {...pageProps} />
     </main>
   );
