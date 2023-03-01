7205457ba274d235ac6b68b1d9bacbe0a8453ee8
wecesscary <wesleymhj@gmail.com>
2023-03-01 19:05:27 +0000

    delete favicon line

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ export default function Home() {
           content="Wesley Jessie is a front end developer based in London."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="font-serif">
         <Hero />
