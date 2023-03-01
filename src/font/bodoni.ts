e937df96aa1d9c6c60c826487f5f2828cc72f6bd
wecesscary <wesleymhj@gmail.com>
2023-03-01 16:51:10 +0000

    update bodoni font

--- src/font/bodoni.ts
+++ src/font/bodoni.ts
@@ -3,42 +3,42 @@ import localFont from "next/font/local";
 export const bodoni = localFont({
   src: [
     {
-      path: "../font/Bodoni-72-Book.otf",
+      path: "../font/Bodoni-24-Book.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../font/Bodoni-72-Book-italic.otf",
+      path: "../font/Bodoni-24-Book-Italic.otf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "../font/Bodoni-72-Medium.otf",
+      path: "../font/Bodoni-24-Medium.otf",
       weight: "500",
       style: "medium",
     },
     {
-      path: "../font/Bodoni-72-Medium-Italic.otf",
+      path: "../font/Bodoni-24-Medium-Italic.otf",
       weight: "500",
       style: "italic",
     },
     {
-      path: "../font/Bodoni-72-Bold.otf",
+      path: "../font/Bodoni-24-Bold.otf",
       weight: "600",
       style: "semibold",
     },
     {
-      path: "../font/Bodoni-72-Bold-Italic.otf",
+      path: "../font/Bodoni-24-Bold-Italic.otf",
       weight: "600",
       style: "italic",
     },
     {
-      path: "../font/Bodoni-72-Fatface.otf",
+      path: "../font/Bodoni-24-Fatface.otf",
       weight: "700",
       style: "bold",
     },
     {
-      path: "../font/Bodoni-72-Fatface-Italic.otf",
+      path: "../font/Bodoni-24-Fatface-Italic.otf",
       weight: "700",
       style: "italic",
     },
