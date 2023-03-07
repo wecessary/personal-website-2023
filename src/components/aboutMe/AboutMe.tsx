dbf62fd9dada92349051e75e86eae4f2728159a4
wecesscary <wesleymhj@gmail.com>
2023-03-07 13:31:35 +0000

    fourth iteration

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -1,19 +1,10 @@
-import {
-  animated,
-  config,
-  useSpring,
-  useChain,
-  useTransition,
-  useSpringRef,
-  useInView,
-} from "@react-spring/web";
-import { useEffect, useState } from "react";
-import useMeasure from "react-use-measure";
+import { pingPong } from "@/const/spring";
+import { animated, config, useSpring, useInView } from "@react-spring/web";
+import { Trail } from "../common/animation/Trail";
+import { PingPoing } from "./PingPong";
+import { Tetris } from "./Tetris";
 
 export const AboutMe = () => {
-  const [squareContainerRef, { width }] = useMeasure();
-  const [pingPongRef, foo] = useMeasure();
-
   const [ref, inView] = useInView({ amount: 0.8 });
   const springs = useSpring({
     from: { backgroundColor: "#FBF9F9", color: "#000000" },
@@ -24,41 +15,6 @@ export const AboutMe = () => {
     config: config.slow,
   });
 
-  const [springs1, api] = useSpring(() => ({
-    from: { x: 0, rotate: 0 },
-  }));
-
-  const [springs2, api2] = useSpring(() => ({
-    from: { x: -(foo.width / 2 - 35), y: 10 },
-  }));
-
-  useEffect(() => {
-    if (inView) {
-      api.start({
-        from: { x: 0, rotate: 0 },
-        to: [
-          { x: width - 64, rotate: 0 },
-          { x: width - 64, rotate: 180 },
-          { x: 0, rotate: 180 },
-          { x: 0, rotate: 0 },
-        ],
-        loop: true,
-        config: config.slow,
-      });
-      api2.start({
-        from: { x: -(foo.width / 2 - 35), y: 10 },
-        to: [
-          { x: foo.width / 2 - 35, y: -10 },
-          { x: -(foo.width / 2 - 35), y: 10 },
-        ],
-        loop: true,
-        config: config.slow,
-      });
-    } else {
-      api.stop();
-      api2.stop();
-    }
-  }, [inView, width, foo.width]);
   return (
     <>
       <animated.div
@@ -66,106 +22,21 @@ export const AboutMe = () => {
         style={{ ...springs }}
         className="flex gap-4 justify-center items-center min-h-screen"
       >
-        <div className="w-[80vw] xl:w-[60vw]">
-          <div className="text-justify  text-2xl md:text-4xl">
+        <div className="w-[80vw] xl:w-[60vw] flex flex-col gap-10">
+          <p className="text-justify text-2xl md:text-4xl">
             Bring your vision to the web with stunning visuals and smooth
             animations.
+          </p>
+          <div className="flex flex-col md:flex-row gap-10 md:gap-0 md:justify-between">
+            <Tetris parentInView={inView} />
+            <PingPoing parentInView={inView} />
           </div>
-          <div className="flex flex-col md:flex-row gap-10 lg:gap-0 justify-between mt-10">
-            <div ref={squareContainerRef}>
-              <animated.div
-                style={{ x: springs1.x }}
-                className="w-16 h-16 bg-background"
-              ></animated.div>
-              <div className="flex justify-between gap-4 mt-4">
-                {Array(3)
-                  .fill(0)
-                  .map((_, index) => (
-                    <animated.div
-                      key={index}
-                      style={{ rotate: springs1.rotate }}
-                      className="w-16 h-16 bg-background"
-                    />
-                  ))}
-              </div>
-            </div>
-            <div>
-              <div ref={pingPongRef} className="flex justify-between gap-4">
-                <animated.div
-                  style={{ y: springs2.y }}
-                  className="bg-background h-36 w-6 mr-20"
-                />
-                <animated.div
-                  style={{ x: springs2.x }}
-                  className="bg-background h-6 w-6 rounded-full mt-12"
-                />
-                <animated.div
-                  style={{ y: springs2.y.to((y) => -y) }}
-                  className="bg-background h-36 w-6 ml-20"
-                />
-              </div>
-            </div>
-          </div>
+          <p className="text-justify text-2xl md:text-4xl">
+            I am a front end developer based in London. I build functional and
+            visually appealing websites.
+          </p>
         </div>
       </animated.div>
     </>
   );
 };
-
-{
-  /* <div className="text-justify px-8 lg:px-0 md:w-3xl md:text-4xl">
-<p>I am a front end developer based in London.</p>
-<p>
-  I
-  <Trail animation={pingPong}>
-    <span className="inline-block px-2">build</span>
-    <span className="inline-block px-2"> functional</span>
-    <span className="inline-block px-2"> and </span>
-    <span className="inline-block px-2">visual</span>
-    <span className="inline-block px-2"> websites.</span>
-  </Trail>
-</p>
-</div> */
-}
-
-// const springs1 = useSpring({
-//   from: { scale: 0.2, backgroundColor: "#715A5A" },
-//   to: [{ scale: 4 }, { backgroundColor: "#ffffff" }],
-//   config: config.slow,
-//   loop: true,
-// });
-
-// const springs2 = useSpring({
-//   from: { x1: 0, x2: 0, y2: 0, scaleX1: 1, scaleX2: 1, scaleY2: 1 },
-//   to: [
-//     { x1: 80, x2: 0, y2: 0, scaleX1: 1, scaleX2: 1, scaleY2: 1 },
-//     { x1: 160, x2: -120, scaleX1: 0.33, scaleX2: 4 },
-//     { scaleY2: 4, y2: -120 },
-//   ],
-//   loop: true,
-// });
-
-{
-  /* <animated.div className="w-80 h-80 bg-background flex items-center justify-center ">
-<animated.div
-  style={{ ...springs1 }}
-  className="h-20 w-20 flex items-center justify-center"
->
-  <animated.div className="h-5 w-5 bg-background flex items-center justify-center" />
-</animated.div>
-</animated.div>
-<div className="relative h-80 w-80">
-<animated.div
-  style={{ scaleX: springs2.scaleX1, x: springs2.x1 }}
-  className="h-60 w-60 bg-background "
-/>
-<animated.div
-  style={{
-    scaleX: springs2.scaleX2,
-    scaleY: springs2.scaleY2,
-    x: springs2.x2,
-    y: springs2.y2,
-  }}
-  className="h-20 w-20 bg-background absolute right-0"
-/> */
-}
---
+++ src/components/aboutMe/Tetris.tsx
@@ -0,0 +1,52 @@
+import { ParentInView } from "@/model/model";
+import { animated, useSpring, config } from "@react-spring/web";
+import { useEffect } from "react";
+import useMeasure from "react-use-measure";
+
+export const Tetris = ({ parentInView }: ParentInView) => {
+  const [ref, { width }] = useMeasure();
+  const slideDistance = width - 64; // 64 is the width of the sliding div
+  const rotation = 180;
+  const [springs1, api] = useSpring(() => ({
+    from: { x: 0, rotate: 0 },
+  }));
+
+  useEffect(() => {
+    if (parentInView) {
+      api.start({
+        from: { x: 0, rotate: 0 },
+        to: [
+          { x: slideDistance, rotate: 0 },
+          { x: slideDistance, rotate: rotation },
+          { x: 0, rotate: rotation },
+          { x: 0, rotate: 0 },
+        ],
+        loop: true,
+        config: config.slow,
+      });
+    } else {
+      api.stop();
+    }
+  }, [parentInView, slideDistance]);
+  return (
+    <>
+      <div ref={ref}>
+        <animated.div
+          style={{ x: springs1.x }}
+          className="w-16 h-16 bg-background"
+        />
+        <div className="flex justify-between gap-4 mt-4">
+          {Array(3)
+            .fill(0)
+            .map((_, index) => (
+              <animated.div
+                key={index}
+                style={{ rotate: springs1.rotate }}
+                className="w-16 h-16 bg-background"
+              />
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
