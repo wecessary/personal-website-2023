f164559bf8bff2f99d9b7c2f900b43477b981d36
wecesscary <wesleymhj@gmail.com>
2023-03-08 15:55:34 +0000

    refactor hero and grid row component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,69 +1,47 @@
-import {
-  appearBottomUp,
-  ballGridLineConfig,
-  gridLineConfig,
-} from "@/const/spring";
-import { wordToLetters } from "@/lib/string/wordToLetters";
 import { Trail } from "../common/animation/Trail";
-import { Ball } from "./Ball";
 import { Row } from "./Grid";
 
 export const Hero = () => {
   return (
-    <section className="grid grid-rows-6 grid-flow-col overflow-hidden">
-      <Row config={gridLineConfig} animateFromLeft border>
-        <h1>
-          <Trail animation={appearBottomUp}>
-            {wordToLetters("WESLEY JESSIE")}
-          </Trail>
-        </h1>
-      </Row>
-      <Row config={gridLineConfig} border textDirection="text-end">
-        <JobTitle />
-      </Row>
-      <Row config={gridLineConfig} border animateFromLeft>
-        <div className="text-lg lg:text-4xl">
-          <Trail animation={appearBottomUp}>
-            {wordToLetters("wesleymhj@gmail.com")}
-          </Trail>
-        </div>
-        <div className="text-lg lg:text-4xl">
-          <Trail animation={appearBottomUp}>
-            {wordToLetters("+447597296544")}
-          </Trail>
-        </div>
-      </Row>
+    <section className="grid grid-rows-5 items-end grid-flow-col overflow-hidden min-h-[90vh] lg:min-h-[98vh] ">
       <Row
-        config={ballGridLineConfig}
+        textSize="hero"
+        animateFromLeft
         border
-        localStyles="flex justify-end h-32"
-      >
-        <Ball />
-      </Row>
-      <Row config={gridLineConfig} animateFromLeft border>
-        <Trail animation={appearBottomUp}>{wordToLetters("LONDON, UK")}</Trail>
-      </Row>
+        renderRowChild={(isRowReady) => (
+          <Trail animationStart={isRowReady}>{["WESLEY", "JESSIE"]}</Trail>
+        )}
+      />
+      <Row
+        textSize="hero"
+        border
+        renderRowChild={(isRowReady) => (
+          <Trail animationStart={isRowReady}>
+            {["FRONT", "END", "DEVELOPER"]}
+          </Trail>
+        )}
+      />
+      <Row
+        textSize="smallHero"
+        border
+        renderRowChild={(isRowReady) => (
+          <Trail animationStart={isRowReady}>{"+44 7597296544"}</Trail>
+        )}
+      />
+      <Row
+        textSize="smallHero"
+        border
+        renderRowChild={(isRowReady) => (
+          <Trail animationStart={isRowReady}>{"wesleymhj@gmail.com"}</Trail>
+        )}
+      />
+      <Row
+        textSize="hero"
+        border
+        renderRowChild={(isRowReady) => (
+          <Trail animationStart={isRowReady}>{["LONDON,", "UK"]}</Trail>
+        )}
+      />
     </section>
   );
 };
-
-const JobTitle = () => (
-  <>
-    <span className="text-sm lg:text-base">
-      <Trail animation={appearBottomUp}>{wordToLetters("(creative)")}</Trail>
-    </span>
-    {/* laptop view */}
-    <Trail animation={appearBottomUp} display="hidden lg:inline-block">
-      {wordToLetters("FRONT END DEVELOPER")}
-    </Trail>
-    {/* mobile view */}
-    <Trail animation={appearBottomUp} display="inline-block lg:hidden">
-      {wordToLetters("FRONT END")}
-    </Trail>
-    <div className="block lg:hidden">
-      <Trail animation={appearBottomUp} display="inline-block lg:hidden">
-        {wordToLetters("DEVELOPER")}
-      </Trail>
-    </div>
-  </>
-);
---
+++ src/const/textSizes.ts
@@ -0,0 +1,4 @@
+export const textSizeClasses = {
+  hero: "text-3xl md:text-5xl lg:text-6xl xl:text-7xl",
+  smallHero: "text-2xl md:text-3xl lg:text-4xl xl:text-5xl",
+};
