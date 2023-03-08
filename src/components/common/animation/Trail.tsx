644ff1ddd2d104655ff94da2561f073046738cb4
wecessary <92378671+wecessary@users.noreply.github.com>
2023-03-08 15:59:16 +0000

    refactor hero and grid row component (#5)
    grid row content now starts animation when grid row is finished animating and the content is rendered before the grid row is finished animating. Before this pr, grid row content is not rendered before grid row is finished animating.

--- src/components/common/animation/Trail.tsx
+++ src/components/common/animation/Trail.tsx
@@ -1,24 +1,35 @@
-import { animated, AnimationProps, useTrail } from "@react-spring/web";
-import { Children, ReactNode } from "react";
+import { animated, AnimationProps, config, useTrail } from "@react-spring/web";
+import { Children, ReactNode, useEffect } from "react";
 
 export const Trail = ({
   children,
   display,
-  animation,
+  animationStart,
 }: {
   children: ReactNode;
   display?: string;
-  animation: AnimationProps;
+  animationStart: boolean;
 }) => {
   const items = Children.toArray(children);
-  const trail = useTrail(items.length, animation);
+  const [trail, api] = useTrail(items.length, () => ({
+    from: { opacity: 0, y: 300 },
+  }));
+
+  useEffect(() => {
+    if (animationStart) {
+      api.start({
+        to: { opacity: 1, y: 0 },
+        config: config.slow,
+      });
+    }
+  }, [animationStart, api]);
 
   return (
     <>
       {trail.map((style, index) => {
         return (
           <animated.div
-            className={`${display || "inline-block"}`}
+            className={`${display || "inline-block"} mx-1 lg:mx-2`}
             key={index}
             style={style}
           >
--- src/components/hero/Grid.tsx
+++ src/components/hero/Grid.tsx
@@ -1,42 +1,48 @@
-import { animated, useSpring } from "@react-spring/web";
+import { textSizeClasses } from "@/const/textSizes";
+import { animated, config, useSpring } from "@react-spring/web";
 import { ReactNode, useState } from "react";
 
-export const Row = ({
-  children,
+interface RowProps<T> {
+  textDirection?: string;
+  textSize: keyof typeof textSizeClasses;
+  border?: boolean;
+  animateFromLeft?: boolean;
+  rowSpan?: string;
+  localStyles?: string;
+  renderRowChild: (isParentReady: boolean) => ReactNode;
+}
+
+export function Row<T>({
   textDirection,
   border,
   animateFromLeft,
   rowSpan,
   localStyles,
-  config,
-}: {
-  children: ReactNode;
-  textDirection?: string;
-  border?: boolean;
-  animateFromLeft?: boolean;
-  rowSpan?: string;
-  localStyles?: string;
-  config: { tension: number; friction: number };
-}) => {
+  textSize,
+  renderRowChild,
+}: RowProps<T>) {
   const [finished, setFinished] = useState(false);
   const springs = useSpring({
     from: { x: animateFromLeft ? "-100vw" : "100vw" },
     to: { x: "0" },
-    config: config,
     onRest() {
       setFinished(true);
     },
+    config: config.slow,
   });
+
   return (
     <>
       <animated.div
         style={{ ...springs }}
         className={`${rowSpan || "row-span-1"} ${localStyles} ${
           border && "border-b-[1px] border-black"
-        } self-end text-4xl md:text-5xl lg:text-7xl overflow-hidden ${textDirection}`}
+        }  overflow-hidden ${textDirection} flex justify-center`}
       >
-        {finished ? children : null}
+        <div className={`w-[80vw] ${textSizeClasses[textSize]} `}>
+          {renderRowChild(finished)}
+        </div>
       </animated.div>
     </>
   );
-};
+}
