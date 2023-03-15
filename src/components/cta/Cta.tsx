dc5e063dc343ca324344b7d3b831ada1984651f8
wecesscary <wesleymhj@gmail.com>
2023-03-15 20:12:12 +0000

    second iteration

--- src/components/cta/Cta.tsx
+++ src/components/cta/Cta.tsx
@@ -11,10 +11,8 @@ import { HackerEffect } from "../common/animation/HackerEffect";
 
 export const Cta = () => {
   const [ref, inView] = useInView({ amount: 0.5 });
-  const [startH, setStartH] = useState(false);
-  const [flip, setFlip] = useState(false);
   const [springs, api] = useSprings(3, (i) => ({
-    from: { x: "100%", y: 0 },
+    from: { x: "40%", y: 0 },
     config: config.gentle,
   }));
 
@@ -22,33 +20,32 @@ export const Cta = () => {
     if (inView) {
       api.start((i) => ({
         to: async (next) => {
-          await next({ y: -4 });
+          await next({ y: -4, delay: 1000 });
           await next({ x: "0%", delay: 1000 });
           await next({ y: 0, delay: 1000 });
-          await next({ x: "100%", delay: 1000 });
+          await next({ x: "40%", delay: 1000 });
         },
         loop: true,
       }));
     } else {
       api.stop();
     }
-  });
-  console.log(startH);
+  }, [inView, api]);
   return (
     <>
-      <div ref={ref} className="min-h-[40vh] flex justify-center">
+      <div ref={ref} className="min-h-[90vh] flex justify-center">
         <div className="w-[90vw] md:w-[80vw] border-r-2 border-l-2 border-black flex flex-col justify-center">
           <div
             className={`${textSizeClasses.sm} flex flex-col items-center gap-4`}
           >
-            <div className="w-[88vw] md:w-[70vw] flex justify-between items-center">
-              <span>YOUR</span>
-              <div>WORDPRESS</div>
-              <span>TEMPLATE</span>
+            <div className="w-[88vw] md:w-[50vw] flex justify-between items-center">
+              <span className="basis-1/3">YOUR</span>
+              <div className="basis-1/3 text-center ">WORDPRESS</div>
+              <span className="basis-1/3 text-end">TEMPLATE</span>
             </div>
-            <div className="w-[88vw] md:w-[70vw] flex justify-between ">
-              <span>NOT</span>
-              <div className="relative overflow-y-hidden">
+            <div className="w-[88vw] md:w-[50vw] flex justify-between ">
+              <span className="basis-1/3 ">NOT</span>
+              <div className="basis-1/3 flex justify-center relative overflow-y-hidden ">
                 <div className="opacity-0">CUTTING</div>
                 <animated.div
                   className="absolute top-0"
@@ -66,22 +63,19 @@ export const Cta = () => {
                   CUTTING
                 </animated.div>
               </div>
-              <span>IT?</span>
+              <span className="basis-1/3 text-end">IT?</span>
             </div>
-            <div className="w-[88vw] md:w-[70vw] flex justify-between ">
-              <animated.div
-                className="basis-1/3 text-start"
-                style={{ x: springs[1].x }}
-              >
+            <div className="w-[88vw] md:w-[50vw] flex justify-between ">
+              <animated.div className="basis-1/3" style={{ x: springs[1].x }}>
                 SIT BACK
               </animated.div>
-              <span className="">AND</span>
-              <span className="">RELAX</span>
+              <span className="basis-1/3 text-center">AND</span>
+              <span className="basis-1/3 text-end">RELAX</span>
             </div>
-            <div className="w-[88vw] md:w-[70vw] flex justify-between ">
-              <span className="basis-1/3 text-start">{"LET'S"}</span>
-              <span className="bais-1/6 text-center">HAVE A</span>
-              <span className="basis-2/6 text-end">CHAT</span>
+            <div className="w-[88vw] md:w-[50vw] flex justify-between ">
+              <span className="basis-1/3 text-start">{"LET"}</span>
+              <span className="bais-1/3 text-center">US</span>
+              <span className="basis-1/3 text-end">CHAT</span>
             </div>
           </div>
         </div>
