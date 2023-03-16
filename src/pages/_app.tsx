92843b3a4d091f5ee6e3f91da9b7f585137f3fd9
wecessary <92378671+wecessary@users.noreply.github.com>
2023-03-16 16:13:38 +0000

    change font to hv-analogue (#15)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import { bodoni } from "@/font/bodoni";
+import { hvAnalogue } from "@/font/hvAnalogue";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${bodoni.variable} font-serif`}>
+    <main className={`${bodoni.variable} ${hvAnalogue.variable}`}>
       <Component {...pageProps} />
     </main>
   );
