00fe52876987f3309de0e986718355ea5a271833
wecesscary <wesleymhj@gmail.com>
2023-03-16 13:29:03 +0000

    third iteration of Cta

---
+++ src/components/common/animation/Rainbow.tsx
@@ -0,0 +1,57 @@
+import { animated, config, useInView, useTrail } from "@react-spring/web";
+import { useEffect } from "react";
+
+export const Rainbow = ({}) => {
+  const [ref, inView] = useInView({ amount: 1 });
+  const text = ["UPGRADE TO", "CUSTOM", "DESGIN AND", "CODE."];
+  const colour = [
+    "f94144",
+    "f3722c",
+    "f8961e",
+    "f9844a",
+    "f9c74f",
+    "90be6d",
+    "43aa8b",
+    "4d908e",
+    "577590",
+    "4d908e",
+  ];
+
+  const colour2 = ["ea8c55", "c75146", "ad2e24", "81171b", "540804"];
+
+  const [trails, api] = useTrail(text.length, () => ({
+    from: { y: 40, opacity: 0 },
+  }));
+
+  useEffect(() => {
+    if (inView) {
+      api.start({
+        reset: true,
+        to: [{ y: 0 }, { opacity: 1 }],
+        config: config.default,
+      });
+    } else {
+      api.stop();
+    }
+  }, [inView, api]);
+  return (
+    <>
+      <div className="flex flex-col" ref={ref}>
+        {trails.map((style, index) => (
+          <div key={index} className="relative overflow-y-hidden">
+            <div>{text[index]}</div>
+            <animated.span
+              style={{
+                ...style,
+                color: `#${colour[index % (colour.length + 1)]}`,
+              }}
+              className="absolute top-0 opacity-70 inline-block"
+            >
+              {text[index]}
+            </animated.span>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
--- src/components/cta/Cta.tsx
+++ src/components/cta/Cta.tsx
@@ -1,100 +1,27 @@
-import { textSizeClasses } from "@/const/textSizes";
-import {
-  animated,
-  config,
-  useInView,
-  useSpring,
-  useSprings,
-} from "@react-spring/web";
-import { useEffect, useState } from "react";
-import { HackerEffect } from "../common/animation/HackerEffect";
+import { Rainbow } from "../common/animation/Rainbow";
+import { RowChildContainer } from "../common/ui/grid/RowContainers";
 
 export const Cta = () => {
-  const [ref, inView] = useInView({ amount: 0.5 });
-  const [springs, api] = useSprings(3, (i) => ({
-    from: { x: "40%", y: 0 },
-    config: config.gentle,
-  }));
-
-  useEffect(() => {
-    if (inView) {
-      api.start((i) => ({
-        to: async (next) => {
-          await next({ y: -4, delay: 1000 });
-          await next({ x: "0%", delay: 1000 });
-          await next({ y: 0, delay: 1000 });
-          await next({ x: "40%", delay: 1000 });
-        },
-        loop: true,
-      }));
-    } else {
-      api.stop();
-    }
-  }, [inView, api]);
   return (
     <>
-      <div ref={ref} className="min-h-[90vh] flex justify-center">
-        <div className="w-[90vw] md:w-[80vw] border-r-2 border-l-2 border-black flex flex-col justify-center">
-          <div
-            className={`${textSizeClasses.sm} flex flex-col items-center gap-4`}
-          >
-            <div className="w-[88vw] md:w-[50vw] flex justify-between items-center">
-              <span className="basis-1/3">YOUR</span>
-              <div className="basis-1/3 text-center ">WORDPRESS</div>
-              <span className="basis-1/3 text-end">TEMPLATE</span>
-            </div>
-            <div className="w-[88vw] md:w-[50vw] flex justify-between ">
-              <span className="basis-1/3 ">NOT</span>
-              <div className="basis-1/3 flex justify-center relative overflow-y-hidden ">
-                <div className="opacity-0">CUTTING</div>
-                <animated.div
-                  className="absolute top-0"
-                  style={{ y: springs[0].y, clipPath: "inset(0 0 50% 0)" }}
-                >
-                  CUTTING
-                </animated.div>
-                <animated.div
-                  className="absolute top-0"
-                  style={{
-                    y: springs[0].y.to((y) => -y),
-                    clipPath: "inset(50% 0 0 0)",
-                  }}
-                >
-                  CUTTING
-                </animated.div>
-              </div>
-              <span className="basis-1/3 text-end">IT?</span>
-            </div>
-            <div className="w-[88vw] md:w-[50vw] flex justify-between ">
-              <animated.div className="basis-1/3" style={{ x: springs[1].x }}>
-                SIT BACK
-              </animated.div>
-              <span className="basis-1/3 text-center">AND</span>
-              <span className="basis-1/3 text-end">RELAX</span>
-            </div>
-            <div className="w-[88vw] md:w-[50vw] flex justify-between ">
-              <span className="basis-1/3 text-start">{"LET"}</span>
-              <span className="bais-1/3 text-center">US</span>
-              <span className="basis-1/3 text-end">CHAT</span>
-            </div>
+      <section className="min-h-[80vh] md:min-h-screen flex justify-center">
+        <RowChildContainer
+          sideBorders
+          textSize="lg"
+          childPosition="flexColCenter"
+          borderColour="border-black"
+        >
+          <div className="text-gray-500">TIRED OF</div>
+          <div className="text-gray-500">GENRIC</div>
+          <div className="text-gray-500">TEMPLATES?</div>
+          <Rainbow />
+          <div className="text-end text-lg md:text-2xl">{"Let's talk."}</div>
+          <div className="text-end text-lg md:text-2xl">
+            wesleymhj@gmail.com
           </div>
-        </div>
-      </div>
-      <div></div>
+          <div className="text-end text-lg md:text-2xl">+44 7597296544</div>
+        </RowChildContainer>
+      </section>
     </>
   );
 };
-
-{
-  /* <span className="overflow-y-hidden basis-1/6 text-center">
-{"CODE".split("").map((char, index) => (
-  <animated.span
-    style={{ y: springs[2].y }}
-    className="inline-block"
-    key={index}
-  >
-    {char}
-  </animated.span>
-))}
-</span> */
-}
