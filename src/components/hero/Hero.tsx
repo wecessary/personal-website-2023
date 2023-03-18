3eec81ec08a3411888c3484c9054165844de0126
wecessary <92378671+wecessary@users.noreply.github.com>
2023-03-18 10:42:53 +0000

    make logo mount when first hero row is ready (#19)

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,77 +1,88 @@
+import { useState } from "react";
+import { CD } from "../CD";
 import { Trail } from "../common/animation/Trail";
 import { Row } from "../common/ui/grid/Row";
 import { RowChildContainer } from "../common/ui/grid/RowContainers";
 
 export const Hero = () => {
+  const [firstRowReady, setFirstRowReady] = useState(false);
   return (
-    <section className="grid grid-rows-5 grid-flow-col overflow-hidden min-h-[90vh] lg:min-h-[98vh] text-orange">
-      <Row
-        animation="animateFromLeft"
-        border
-        renderRowChild={(isRowReady) => (
-          <RowChildContainer
-            childPosition="touchBtBorder"
-            textSize="xl"
-            fontBold
-          >
-            <Trail animationStart={isRowReady}>{["WESLEY", "JESSIE"]}</Trail>
-          </RowChildContainer>
-        )}
-      />
-      <Row
-        border
-        animation="animateFromRight"
-        renderRowChild={(isRowReady) => (
-          <RowChildContainer
-            childPosition="touchBtBorder"
-            textSize="xl"
-            fontBold
-          >
-            <Trail animationStart={isRowReady}>
-              {["FRONT", "END", "DEVELOPER"]}
-            </Trail>
-          </RowChildContainer>
-        )}
-      />
-      <Row
-        border
-        animation="animateFromLeft"
-        renderRowChild={(isRowReady) => (
-          <RowChildContainer
-            childPosition="touchBtBorder"
-            textSize="md"
-            fontBold
-          >
-            <Trail animationStart={isRowReady}>{"+44 7597296544"}</Trail>
-          </RowChildContainer>
-        )}
-      />
-      <Row
-        animation="animateFromRight"
-        border
-        renderRowChild={(isRowReady) => (
-          <RowChildContainer
-            childPosition="touchBtBorder"
-            textSize="md"
-            fontBold
-          >
-            <Trail animationStart={isRowReady}>{"wesleymhj@gmail.com"}</Trail>
-          </RowChildContainer>
-        )}
-      />
-      <Row
-        animation="animateFromLeft"
-        border
-        renderRowChild={(isRowReady) => (
-          <RowChildContainer
-            childPosition="touchBtBorder"
-            textSize="xl"
-            fontBold
-          >
-            <Trail animationStart={isRowReady}>{["LONDON,", "UK"]}</Trail>
-          </RowChildContainer>
-        )}
-      />
-    </section>
+    <>
+      {firstRowReady && <CD />}
+      <section className="grid grid-rows-5 grid-flow-col overflow-hidden min-h-[90vh] lg:min-h-[98vh] text-orange">
+        <Row
+          animation="animateFromLeft"
+          border
+          renderRowChild={(isRowReady) => {
+            setFirstRowReady(isRowReady);
+            return (
+              <RowChildContainer
+                childPosition="touchBtBorder"
+                textSize="xl"
+                fontBold
+              >
+                <Trail animationStart={isRowReady}>
+                  {["WESLEY", "JESSIE"]}
+                </Trail>
+              </RowChildContainer>
+            );
+          }}
+        />
+        <Row
+          border
+          animation="animateFromRight"
+          renderRowChild={(isRowReady) => (
+            <RowChildContainer
+              childPosition="touchBtBorder"
+              textSize="xl"
+              fontBold
+            >
+              <Trail animationStart={isRowReady}>
+                {["FRONT", "END", "DEVELOPER"]}
+              </Trail>
+            </RowChildContainer>
+          )}
+        />
+        <Row
+          border
+          animation="animateFromLeft"
+          renderRowChild={(isRowReady) => (
+            <RowChildContainer
+              childPosition="touchBtBorder"
+              textSize="md"
+              fontBold
+            >
+              <Trail animationStart={isRowReady}>{"+44 7597296544"}</Trail>
+            </RowChildContainer>
+          )}
+        />
+        <Row
+          animation="animateFromRight"
+          border
+          renderRowChild={(isRowReady) => (
+            <RowChildContainer
+              childPosition="touchBtBorder"
+              textSize="md"
+              fontBold
+            >
+              <Trail animationStart={isRowReady}>{"wesleymhj@gmail.com"}</Trail>
+            </RowChildContainer>
+          )}
+        />
+        <Row
+          animation="animateFromLeft"
+          border
+          renderRowChild={(isRowReady) => (
+            <RowChildContainer
+              childPosition="touchBtBorder"
+              textSize="xl"
+              fontBold
+            >
+              <Trail animationStart={isRowReady}>{["LONDON,", "UK"]}</Trail>
+            </RowChildContainer>
+          )}
+        />
+      </section>
+    </>
   );
 };
